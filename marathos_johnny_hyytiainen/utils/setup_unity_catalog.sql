942a40fda78c858c6c381d4b4d419d64fb77a6d4
JohnnyHyytiainen <johnny.hyytiainen@gmail.com>
2026-05-23 12:40:55 +0000

    feat: Unity Catalog setup, created volume, schemas and catalog for Marathos lab

---
+++ marathos_johnny_hyytiainen/utils/setup_unity_catalog.sql
@@ -0,0 +1,13 @@
+-- Skapa huvudkatalogen
+CREATE CATALOG IF NOT EXISTS marathos;
+USE CATALOG marathos;
+
+-- Skapa scheman för alla 3 medallion-lager
+CREATE SCHEMA IF NOT EXISTS bronze;
+CREATE SCHEMA IF NOT EXISTS silver;
+CREATE SCHEMA IF NOT EXISTS gold;
+
+-- Skapa default-schemat för raw data
+CREATE SCHEMA IF NOT EXISTS default;
+
+CREATE VOLUME IF NOT EXISTS default.raw;
