b18c16e500e9cde910829bd35ed137d61e173fdd
JohnnyHyytiainen <johnny.hyytiainen@gmail.com>
2026-05-28 18:13:15 +0000

    feat: gold layer complete with dim and fact tables written purely in PySpark.
    - Cleaned up setup of unity catalog,
    - Updated with sql folder to write my spark logic but with SQL
    - Updated gold layer pipe script with pure PySpark to include fact and dimensional tables

--- marathos_johnny_hyytiainen/explorations/setup_unity_catalog.sql
+++ marathos_johnny_hyytiainen/explorations/setup_unity_catalog.sql
@@ -7,10 +7,5 @@ CREATE SCHEMA IF NOT EXISTS bronze;
 CREATE SCHEMA IF NOT EXISTS silver;
 CREATE SCHEMA IF NOT EXISTS gold;
 
--- Skapa default-schemat för raw data
-CREATE SCHEMA IF NOT EXISTS default;
 CREATE VOLUME IF NOT EXISTS default.raw;
 
--- Skapa volymer för streaming-infrastruktur
--- CREATE VOLUME IF NOT EXISTS default.checkpoints;
--- CREATE VOLUME IF NOT EXISTS default.schemas;
