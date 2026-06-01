05cfc11b9a077cc3ca73f91de3a7845a267156d3
JohnnyHyytiainen <johnny.hyytiainen@gmail.com>
2026-06-01 12:50:20 +0000

    refactor(marts): refactored global mart script to include iso_code for ease of use with choropleth map in plotly

--- marathos_johnny_hyytiainen/transformations/gold/mart_global_reach.sql
+++ marathos_johnny_hyytiainen/transformations/gold/mart_global_reach.sql
@@ -5,6 +5,7 @@ USE SCHEMA gold;
 CREATE OR REFRESH MATERIALIZED VIEW marathos.gold.mart_global_reach
   COMMENT "Serving view - Gold layer, athlete statistics aggregated by home country" AS
 SELECT
+  c.iso_code,
   c.country_name,
   COUNT(f.result_id) AS total_participants,
   ROUND(AVG(f.athlete_average_speed), 2) AS national_avg_speed_kmh
@@ -15,4 +16,5 @@ FROM
     JOIN marathos.gold.dim_country c
       ON a.athlete_country = c.iso_code
 GROUP BY
+  c.iso_code,
   c.country_name;
