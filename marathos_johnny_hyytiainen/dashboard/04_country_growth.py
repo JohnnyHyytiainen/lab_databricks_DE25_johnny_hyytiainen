197e09b0d376b44b25d4a1c851dc56b644884685
JohnnyHyytiainen <johnny.hyytiainen@gmail.com>
2026-06-01 11:03:46 +0000

    chore(setup): updated repo with empty files to fill in with graphs

---
+++ marathos_johnny_hyytiainen/dashboard/04_country_growth.py
@@ -0,0 +1,6 @@
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+# Script för att få insights över hur länderna växer när det kommer till antal events i datasettet
+# Läsa in data ifrån min mart_global_growth
+
---
+++ marathos_johnny_hyytiainen/dashboard/06_global_reach.py
@@ -0,0 +1,5 @@
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+# Script för att få insights över hur atleterna presterar och vilken global reach atleter har
+# Läsa in data ifrån min mart_global_reach
