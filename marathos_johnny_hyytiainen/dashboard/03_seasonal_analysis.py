1b7806643878ee1da62f2b336c0af803e9f08631
JohnnyHyytiainen <johnny.hyytiainen@gmail.com>
2026-06-01 10:44:42 +0000

    feat(seasonal graph): added seasonal analysis with total events for each nation in notebook. Lifting over to script next

---
+++ marathos_johnny_hyytiainen/dashboard/03_seasonal_analysis.py
@@ -0,0 +1,6 @@
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+# Script för att få en översikt av mina seasonal trends
+# Läsa in data ifrån min mart_seasonal_events_country
+
